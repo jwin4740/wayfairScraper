b0be40e1ee74206009f69b921938be4c9436ff32
James Winkle <jwin4740@gmail.com>
2017-08-02 11:14:35 -0400

    got sizes

--- functions.js
+++ functions.js
@@ -8,6 +8,7 @@ const supplierElement = "a.ProductDetailInfoBlock-header-manuLink";
 const priceElement = "span#sale_price";
 const skuElement = "select.js-select-category.ProductDetail-select-category";
 const colorElement = '.ProductDetail-select-category[data-category="Color"]';
+const sizeElement = '.ProductDetail-select-category[data-category="Size"]';
 
 module.exports = {
 
@@ -36,25 +37,37 @@ module.exports = {
         var sizesArray = []; // cheerio results are stored in here
         var exportedArray = [];
 
-        function SizeConstruct(size, price) {
-            this.Size = size;
-            this.Price = price;
-        }
-        var currentPrice = $(".ProductDetailInfoBlock-pricing-amount").children().text().trim();
-        $("select.ProductDetailOptions-select").children().each(function (i, elem) {
-            var temp = $(this).text().trim();
-            if (temp != "Select Size") {
-                sizesArray.push(temp)
-            }
+        // function SizeConstruct(size, price) {
+        //     this.Size = size;
+        //     this.Price = price;
+        // }
+        // var size = $(".ProductDetailInfoBlock-pricing-amount").children().text().trim();
+        // $("select.ProductDetailOptions-select").children().each(function (i, elem) {
+        //     var temp = $(this).text().trim();
+        //     if (temp != "Select Size") {
+        //         sizesArray.push(temp)
+        //     }
 
-        });
-        return sizesArray;
+        // });
+        // return sizesArray;
         // sizesArray.forEach(function (element) {
         //     var sizeConstruct = new SizeConstruct(element, currenciesAsNumbers);
         //     exportedArray.push(sizeConstruct);
 
         // }, this);
         // return exportedArray;
+
+        $(sizeElement).children().each(function (i, elem) {
+            var temp = $(this).text().trim();
+            //     if (temp != "Select Size") {
+            //         sizesArray.push(temp)
+            //     }
+            var temp = $(this).attr("data-option-name");
+            if (typeof temp != "undefined") {
+                sizesArray.push(temp);
+            }
+        });
+        return sizesArray;
     },
 
     QueryResultLength: function ($) {
